3f24bd818855d96989c7a56fccf74c84e56364dd
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2019-09-27 18:17:28 -0300

    building indecision app with components

--- src/app.js
+++ src/app.js
@@ -1,55 +1,51 @@
-console.log("App.js is runing!");
-
-const app = {
-  title: "Indecision App",
-  subtitle: "Put your life in the hands of a computer",
-  options: []
-};
-
-const onFormSubmit = event => {
-  event.preventDefault();
-
-  const option = event.target.elements.option.value;
-
-  if (option) {
-    app.options.push(option);
-    event.target.elements.option.value = "";
-    render();
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Indecision</h1>
+        <h2>Put your life in the hands of a computer</h2>
+      </div>
+    );
   }
-};
+}
 
-const removeAllOptions = () => {
-  app.options = [];
-  render();
-};
+class Action extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>What should I do?</button>
+      </div>
+    );
+  }
+}
 
-const appRoot = document.querySelector("div#app");
+class Options extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Options component here</p>
+      </div>
+    );
+  }
+}
 
-const render = () => {
-  // JSX - JavaScript XML
-  const template = (
-    <div>
-      <h1>{app.title}</h1>
-      {app.subtitle && <p>{app.subtitle}</p>}
-      <p>
-        {app.options && app.options.length > 0
-          ? "Here are your options"
-          : "No options"}
-      </p>
-      <p>{app.options.length}</p>
-      <button onClick={removeAllOptions}>Remove All</button>
-      <ol>
-        <li>Item one</li>
-        <li>Item two</li>
-      </ol>
-      <form onSubmit={onFormSubmit}>
-        <input type="text" name="option" />
-        <button type="submit">Add Option</button>
-      </form>
-    </div>
-  );
+class AddOption extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>AddOption component here</p>
+      </div>
+    );
+  }
+}
 
-  ReactDOM.render(template, appRoot);
-};
+const jsx = (
+  <div>
+    <Header />
+    <Action />
+    <Options />
+    <AddOption />
+  </div>
+);
 
-render();
+ReactDOM.render(jsx, document.getElementById("app"));
